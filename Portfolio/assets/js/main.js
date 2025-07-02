44356b8bbd7764394de37c772e2797298b15c4ec
PranayMittal00 <pranaymittal2004@gmail.com>
2025-07-03 00:34:03 +0530

    commit

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -26,11 +26,24 @@ navToggle.addEventListener('click', () => {
 /*=============== EMAIL JS ===============*/
 
 /*=============== STYLE SWITCHER ===============*/
+const styleSwitcher = document.getElementById('style-switcher'),
+switcherToggle = document.getElementById('switcher-toggle'),
+switcherClose = document.getElementById('switcher-close');
 
 /* Switcher show */
-
+switcherToggle.addEventListener('click', () => {
+    styleSwitcher.classList.add('show-switcher');
+});
 /* Switcher hidden */
-
+switcherClose.addEventListener('click', () => {
+    styleSwitcher.classList.remove('show-switcher');
+});
 /*=============== THEME COLORS ===============*/
+const colors = document.querySelectorAll('.style-switcher-color');
 
+colors.forEach((color) => {
+    color.onclick = () => {
+        const activeColor = color.style.getPropertyValue('--hue');
+    };
+})
 /*=============== LIGHT/DARK MODE ===============*/
